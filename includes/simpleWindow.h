c1ef9b1ab7c4d5f7e3c2b96810619c17ba3e327a
Thomas <semoir.dense-0h@icloud.com>
2023-09-22 11:36:35 +0900

    add get mouse pos

--- includes/simpleWindow.h
+++ includes/simpleWindow.h
@@ -6,20 +6,27 @@
 /*   By: tchoquet <tchoquet@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 14:32:15 by tchoquet          #+#    #+#             */
-/*   Updated: 2023/09/21 18:40:56 by tchoquet         ###   ########.fr       */
+/*   Updated: 2023/09/22 00:37:28 by tchoquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SIMPLEWINDOW_H
 # define SIMPLEWINDOW_H
 
-typedef struct s_window	t_window;
+typedef struct s_window		t_window;
+typedef struct s_mouse_pos	t_mouse_pos;
+
+struct s_mouse_pos
+{
+	int	x;
+	int	y;
+};
 
 t_window	*new_window(const char *title, unsigned int width, unsigned int height);
 void		*get_pixel_buffer(t_window *window);
 void		delete_window(t_window *window);
-
 void		start_main_loop(void (*func)(void *), void *data);
+t_mouse_pos	get_mouse_pos(t_window *window);
 
 char		*swin_strerr(void);
 
---
+++ sources/mouse.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mouse.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tchoquet <tchoquet@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/22 00:37:37 by tchoquet          #+#    #+#             */
+/*   Updated: 2023/09/22 00:39:43 by tchoquet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "simpleWindow_intenal.h"
+
+t_mouse_pos	get_mouse_pos(t_window *window)
+{
+	t_mouse_pos pos;
+
+	mlx_mouse_get_pos(window->mlx_win, &pos.x, &pos.y);
+	return (pos);
+}
