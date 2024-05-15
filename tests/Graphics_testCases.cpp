d45eb12433835947f81ea48ef7a64047f7b4f921
Thomas <tchoquet@student.42tokyo.jp>
2024-05-15 14:04:57 +0900

    initial commit

---
+++ tests/Graphics_testCases.cpp
@@ -0,0 +1,10 @@
+/*
+ * ---------------------------------------------------
+ * Graphics_testCases.cpp
+ *
+ * Author: Thomas Choquet <thomas.publique@icloud.com>
+ * Date: 2024/05/15 13:53:46
+ * ---------------------------------------------------
+ */
+
+#include <gtest/gtest.h>
