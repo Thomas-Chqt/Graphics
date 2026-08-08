f509484e417c3f2665d0f7cb687daefff149be30
Thomas Choquet <semoir.dense-0h@icloud.com>
2026-08-09 00:28:17 +0900

    fix parametter block pool size

--- examples/pbr/pbr.cpp
+++ examples/pbr/pbr.cpp
@@ -43,6 +43,7 @@
 #include <stdexcept>
 #include <vector>
 #include <bit>
+#include <functional>
 
 #if __XCODE__
     #include <unistd.h>
@@ -530,12 +531,14 @@ public:
         [[maybe_unused]] const auto glfwResult = glfwInit();
         assert(glfwResult == GLFW_TRUE);
 
+	m_glfwGuard = { (void*)1, [](void*){glfwTerminate();} };
+
         glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-        m_window = glfwCreateWindow(windowWidth, windowHeight, "PBR", nullptr, nullptr);
+        m_window = { glfwCreateWindow(windowWidth, windowHeight, "PBR", nullptr, nullptr), [](GLFWwindow* ptr){glfwDestroyWindow(ptr);} };
         assert(m_window);
 
-        glfwSetWindowUserPointer(m_window, this);
-        glfwSetWindowSizeCallback(m_window, [](GLFWwindow* window, int, int) {
+        glfwSetWindowUserPointer(m_window.get(), this);
+        glfwSetWindowSizeCallback(m_window.get(), [](GLFWwindow* window, int, int) {
             static_cast<Application*>(glfwGetWindowUserPointer(window))->m_swapchain = nullptr;
         });
 
@@ -544,7 +547,7 @@ public:
         });
         assert(m_instance);
 
-        m_surface = gfx::glfw::createSurface(*m_instance, m_window);
+        m_surface = gfx::glfw::createSurface(*m_instance, m_window.get());
         assert(m_surface);
 
         m_device = m_instance->newDevice(gfx::Device::Descriptor{
@@ -722,10 +725,10 @@ public:
         switch (m_device->backend())
         {
         case gfx::Backend::vulkan:
-            ImGui_ImplGlfw_InitForVulkan(m_window, true);
+            ImGui_ImplGlfw_InitForVulkan(m_window.get(), true);
             break;
         default:
-            ImGui_ImplGlfw_InitForOther(m_window, true);
+            ImGui_ImplGlfw_InitForOther(m_window.get(), true);
             break;
         }
 
@@ -740,12 +743,12 @@ public:
         while (true)
         {
             ::glfwPollEvents();
-            if (::glfwWindowShouldClose(m_window))
+            if (::glfwWindowShouldClose(m_window.get()))
                 break;
 
             if (m_swapchain == nullptr) {
                 int width = 0, height = 0;
-                ::glfwGetFramebufferSize(m_window, &width, &height);
+                ::glfwGetFramebufferSize(m_window.get(), &width, &height);
                 m_swapchain = m_device->newSwapchain(gfx::Swapchain::Descriptor{
                     .surface = m_surface.get(),
                     .width = static_cast<uint32_t>(width),
@@ -777,7 +780,7 @@ public:
             }
 
             int width = 0, height = 0;
-            ::glfwGetFramebufferSize(m_window, &width, &height);
+            ::glfwGetFramebufferSize(m_window.get(), &width, &height);
             constexpr glm::vec3 camPos = glm::vec3(0.0f, 0.0f,  3.0f);
             constexpr glm::vec3 camDir = glm::vec3(0.0f, 0.0f, -1.0f);
             constexpr glm::vec3 camUp  = glm::vec3(0.0f, 1.0f,  0.0f);
@@ -894,12 +897,11 @@ public:
         gfx::imgui::shutdown(*m_device);
         ImGui_ImplGlfw_Shutdown();
         ImGui::DestroyContext();
-        glfwDestroyWindow(m_window);
-        glfwTerminate();
     }
 
 private:
-    GLFWwindow* m_window = nullptr;
+    std::unique_ptr<void, std::function<void(void*)>> m_glfwGuard;
+    std::unique_ptr<GLFWwindow, std::function<void(GLFWwindow*)>> m_window;
     std::unique_ptr<gfx::Instance> m_instance;
     std::unique_ptr<gfx::Surface> m_surface;
     std::unique_ptr<gfx::Device> m_device;
